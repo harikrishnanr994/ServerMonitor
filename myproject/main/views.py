c0892e3e4f308e9241d70d4bfa685449cafce2f8
MySachin2 <sachingiridhar@gmail.com>
2017-12-17 08:10:38 +0000

    Merge

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -6,4 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index_main.html')
+
+def runscript(request):
+    email = request.GET['email']
+    username = request.GET['username']
+    host = request.GET['host']
+    domain = request.GET['domain']
+    return render(request, 'run_script.html',{"email" : email, "username" : username, "host" : host,"domain" : domain})
+
