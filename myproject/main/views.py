f13a5b2003c78144b36550989dd3f713136c601f
Sachin Giridhar <sachingiridhar@gmail.com>
2017-12-20 10:59:26 +0530

    Initial App Deployed

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -10,5 +10,9 @@ def index(request):
     return render(request, 'index_main.html')
 
 def runscript(request):
-    if request.method == 'POST':
-        return "SSSS"
+    email = request.GET['email']
+    username = request.GET['username']
+    host = request.GET['host']
+    domain = request.GET['domain']
+    password = request.GET['password']
+    return render(request, 'run_script.html',{"email" : email, "username" : username, "host" : host,"domain" : domain , "password" : password})
