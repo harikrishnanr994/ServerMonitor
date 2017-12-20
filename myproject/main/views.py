a8036555ad36faa9908259b3492e680521f0314a
Sachin Giridhar <sachingiridhar@gmail.com>
2017-12-20 10:48:09 +0530

    Initial App Deployed

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -12,8 +12,4 @@ def index(request):
 @csrf_exempt
 def runscript(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        host = request.POST.get['host']
-        domain = request.POST.get['domain']
-        password = request.POST.get['password']
-        return render(request, 'run_script.html',{ "username" : username, "host" : host,"domain" : domain , "password" : password})
+        return "SSSS"
