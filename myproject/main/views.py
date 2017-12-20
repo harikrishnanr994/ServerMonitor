f081be15174a33d351b17775aa489de85f3b7d00
Sachin Giridhar <sachingiridhar@gmail.com>
2017-12-20 10:35:02 +0530

    Initial App Deployed

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index_main.html')
 
+@csrf_exempt
 def runscript(request):
     if request.method == 'POST':
         email = request.POST.get('email','')
