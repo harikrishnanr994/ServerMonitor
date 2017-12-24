3d15d7f24fe3a4f1db3800e67475089a5f30c7a1
Sachin Giridhar <sachingiridhar@gmail.com>
2017-12-24 18:41:11 +0530

    Some styling in Bootstrap

---
+++ config_files/temp_secret.php
@@ -0,0 +1,8 @@
+define('AUTH_KEY',         'uP|XFF=Gu6Iq-jZ=N,m?%@_|Ec-V? (bY6#B[#.y*@8(!+HW(]n/>JsYXQ>x}X}-');
+define('SECURE_AUTH_KEY',  '!:VLoOFJUTe^elxt4V-nSp`f*}RQz!Ptxh~|w~|bO|cBxn:l88lg%PuC0.b%x(1_');
+define('LOGGED_IN_KEY',    '0goD)AWM8J<%z8=R)g_HZGZl zr,{)g?F<@kY-Yzn|{z`Iy{*xL+!sx6*(*q!:+,');
+define('NONCE_KEY',        't+PDU7Czx.g@!C+CZz_kufK6d@y1-7DD^}dh0c#r3enp)p]sAfPdLs-c>At~!IQv');
+define('AUTH_SALT',        'dQ]a6C^?)]-5IIb=rVCp(^{hwd73YiJ8G,/n+ox,Q{_9{!_8wy^lQH~fO8?^X?;m');
+define('SECURE_AUTH_SALT', 'P1h4e;Vrz,o=kCna*c3M[[C-J%6R5|F<_Hw{IswG>&<I(.8{ u6TVhCgh#1 hu.X');
+define('LOGGED_IN_SALT',   'xx)m6<y`9:Z8Op&`dZ2Yvz-)CR?%e|zw))Jl9<1iwZ/^}lhCQz0nG-4u7s18R{md');
+define('NONCE_SALT',       'bkKrYmJV6()A=ypXZ7lO!8oD<=|)Z#a>dg$c9C|s8 <+h NqoOmj(QRl#=#^YM^X');
--- temp_secret.php
+++
@@ -1,8 +0,0 @@
-define('AUTH_KEY',         'qI4s`@yq32Z0zk]uv-cik:J8(J}8t,jDk+kls&DtrUBRDv?922Nhs,S+)sy[q*ut');
-define('SECURE_AUTH_KEY',  ';):vPe+:pW0I*Y9QsRy`(U3B4 cEe-9gxr&B+INo<1;bD:X3t3e/(];d8OnTucD<');
-define('LOGGED_IN_KEY',    '1H9o,[DfVB1FBz+jj&|<O-aFroHoA7weHs@a=-B=v9bzA00GyA+WGh(a<qts<|!s');
-define('NONCE_KEY',        'nFNwS9ZEk&^2~Tg6QKDLur8MJ16Q)W=kJP)mr|@;2pGLX=Y@GRI[/W9D~-/I,+|a');
-define('AUTH_SALT',        'tt*x.d0{SE%-pRR|[ Uc%*9mU Pe2;Q}I+[|-YD0iP#;1@>D-2z(Y~|Nbh+NDjtY');
-define('SECURE_AUTH_SALT', 'd&drcB{4i21@j1?4QPpf;7v#bw&~gXWCz:Vf=:<`t|E#7>2h#`Bj0]U7pu?+h@dM');
-define('LOGGED_IN_SALT',   'Ze:,J(9y{njXLGbBcqycU~||+p)pvQb.d;~D{uqqGgu~<*iYRE(|VZt2flg~NTyh');
-define('NONCE_SALT',       'qvGhm $-@%J~|E76?+&Eybh`(<-HDSmJ;-pmrdeiaF>[flNJ-Y3~_zRBO2`NkS2Q');
